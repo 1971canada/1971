6048c50098999ab3fd22d0ef550e2b30eb959b84
Louis Roy <louisroy@louisroy.net>
2014-11-23 11:33:36 -0500

    heroku stuff

---
+++ web/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>1971</title>
+	</head>
+	<body>
+		<header>
+			<h1>1971</h1>
+		</header>
+		
+		<div class="content">
+			
+		</div>
+		
+		<footer>
+			
+		</footer>
+	</body>
+</html>
