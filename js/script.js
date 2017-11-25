afe4b830b71c8b64febc2acb0545b240b86ce6af
Louis Roy <louisroy@louisroy.net>
2017-11-25 10:20:20 -0500

    fixing script

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 var $main = $('.main');
 var $return = $('.return');
+var $video = $('.video');
+
+$(window).on('resize load', function (ev) {
+	console.log(window.innerWidth, window.innerHeight);
+	if (window.innerWidth / window.innerHeight < 16 / 9) {
+		$video.css({
+			width:'auto',
+			height:window.innerHeight
+		});
+	} else {
+		$video.css({
+			width:window.innerWidth,
+			height:'auto'
+		});
+	}
+}).trigger('resize');
 
 $(window).on('scroll load', function (ev) {
 	var position = window.scrollY / document.documentElement.clientHeight;
 	$main.css('opacity', Math.max(1 - position, 0.5));
 	$return.css('opacity', Math.min(position, 1));
-});
+}).trigger('scroll');
 
 $(document).on('click', 'a[href^="#"]', function (ev) {
 	ev.preventDefault();
